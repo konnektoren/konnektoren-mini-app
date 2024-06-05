028a14feceba797242aeb4ad9669dd98cc4bfb0c
Christian M <chriamue@gmail.com>
2024-06-05 21:51:34 +0200

    :lipstick: styles buttons

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ pub mod telegram;
 
 pub mod prelude {
     pub use crate::app::App;
-    pub use crate::footer::Footer;
     pub use crate::challenge::{ChallengeComp, ChallengeCompProps};
+    pub use crate::footer::Footer;
 }
