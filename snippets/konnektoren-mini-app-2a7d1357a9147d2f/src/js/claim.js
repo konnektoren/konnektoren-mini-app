0aec4175e73a84d1fdabba04d4d468d485c9fc5f
chriamue <chriamue@users.noreply.github.com>
2024-06-09 09:56:16 +0000

    deploy: e7e07eda02d53f2b6344f6088145c4f340f67cba

--- snippets/konnektoren-mini-app-2a7d1357a9147d2f/src/js/claim.js
+++ snippets/konnektoren-mini-app-2a7d1357a9147d2f/src/js/claim.js
@@ -14,5 +14,35 @@ export async function claim(address, amount) {
 
   console.log("Claiming data", data);
 
-  Telegram.WebApp.sendData(JSON.stringify(data));
+  try {
+    const response = await fetch(
+      "https://tg-api.konnektoren.help/api/v1/claim",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const result = await response.json();
+
+    if (result.success) {
+      Telegram.WebApp.sendData(JSON.stringify(result));
+      alert(`Success! View transaction: ${result.explorer_url}`);
+      return Promise.resolve(result);
+    } else {
+      alert("Claim failed.");
+      return Promise.reject("Claim failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error occurred while claiming.");
+    return Promise.reject(error);
+  }
 }
