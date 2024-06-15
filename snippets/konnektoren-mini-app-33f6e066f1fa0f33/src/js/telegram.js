1749562764ad8a2c7490ec2c54519594a570b6aa
chriamue <chriamue@users.noreply.github.com>
2024-06-15 11:06:14 +0000

    deploy: 89172bac2767797520ea9622f72b3c101256c7d8

--- snippets/konnektoren-mini-app-33f6e066f1fa0f33/src/js/telegram.js
+++ snippets/konnektoren-mini-app-33f6e066f1fa0f33/src/js/telegram.js
@@ -11,19 +11,30 @@ export function tma_set_header_color(color) {
   return Telegram.WebApp.setHeaderColor(color);
 }
 
-export function tma_setItem(key, value, callback) {
-  if (typeof Telegram.WebApp.cloudStorage.setItem !== "function") {
-    callback({ message: "setItem is undefined" }, null);
-    return;
-  }
-  Telegram.WebApp.cloudStorage.setItem(key, value, callback);
-}
-
-export function tma_getItem(key) {
+export function tma_set_item(key, value) {
   return new Promise((resolve, reject) => {
     if (
-      typeof Telegram.WebApp.cloudStorage === "undefined" ||
-      typeof Telegram.WebApp.cloudStorage.getItem !== "function"
+        typeof Telegram.WebApp.cloudStorage === "undefined" ||
+        typeof Telegram.WebApp.cloudStorage.setItem !== "function"
+    ) {
+      reject(new Error("setItem is undefined"));
+      return;
+    }
+    Telegram.WebApp.cloudStorage.setItem(key, value, (error, value) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(value);
+      }
+    });
+  });
+}
+
+export function tma_get_item(key) {
+  return new Promise((resolve, reject) => {
+    if (
+        typeof Telegram.WebApp.cloudStorage === "undefined" ||
+        typeof Telegram.WebApp.cloudStorage.getItem !== "function"
     ) {
       reject(new Error("getItem is undefined"));
       return;
