3d388862261bd440e11f15c811669332fd8cceca
Christian M <chriamue@gmail.com>
2024-06-16 18:20:41 +0200

    :construction_worker: add claim v2 which lets frontend wallet make the transaction

--- src/claim.rs
+++ src/claim.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::{js_sys::Promise, JsFuture};
 use yew::prelude::*;
+use serde::Deserialize;
+use gloo_utils::format::JsValueSerdeExt;
 
 #[wasm_bindgen(module = "/src/js/confetti.js")]
 extern "C" {
@@ -10,6 +12,16 @@ extern "C" {
 #[wasm_bindgen(module = "/src/js/claim.js")]
 extern "C" {
     fn claim(address: &str, amount: u32) -> Promise;
+}
+
+#[wasm_bindgen(module = "/src/js/claim.js")]
+extern "C" {
+    fn claim_v2(address: &str, amount: u32) -> Promise;
+}
+
+#[wasm_bindgen(module = "/src/js/wallet.js")]
+extern "C" {
+    fn sendRawTransaction(raw_tx: &str, destination: &str) -> Promise;
 }
 
 #[derive(Properties, PartialEq)]
@@ -25,13 +37,27 @@ enum ClaimState {
     Error(String),
 }
 
+#[allow(dead_code)]
+enum ClaimVersion {
+    V1,
+    V2,
+}
+
+#[derive(Deserialize)]
+struct RawTransaction {
+    success: bool,
+    raw_transaction: String,
+    destination: String,
+}
+
 #[function_component(ClaimComp)]
 pub fn claim_comp(props: &ClaimCompProps) -> Html {
     let address = props.address.clone();
     let amount = props.amount;
     let claim_state = use_state(|| ClaimState::Initial);
+    const CLAIM_VERSION: ClaimVersion = ClaimVersion::V1;
 
-    let on_claim = {
+    let on_claim: Callback<MouseEvent> = {
         let address = address.clone();
         let claim_state = claim_state.clone();
 
@@ -48,16 +74,59 @@ pub fn claim_comp(props: &ClaimCompProps) -> Html {
             let claim_state = claim_state.clone();
 
             wasm_bindgen_futures::spawn_local(async move {
-                match JsFuture::from(claim(&address, amount)).await {
-                    Ok(result) => {
-                        let explorer_url = result.as_string().unwrap_or_default();
-                        log::info!("Claim successful: {}", explorer_url);
-                        claim_state.set(ClaimState::Done(explorer_url));
+
+                match CLAIM_VERSION {
+                    ClaimVersion::V1 => {
+                        match JsFuture::from(claim(&address, amount)).await {
+                            Ok(result) => {
+                                log::info!("Claim successful: {:?}", result);
+                                let explorer_url = result.as_string().unwrap_or_default();
+                                log::info!("Claim successful: {}", explorer_url);
+                                claim_state.set(ClaimState::Done(explorer_url));
+                            }
+                            Err(e) => {
+                                let error_message = format!("{:?}", e);
+                                log::error!("Claim failed: {}", error_message);
+                                claim_state.set(ClaimState::Error(error_message));
+                            }
+                        }
                     }
-                    Err(e) => {
-                        let error_message = format!("{:?}", e);
-                        log::error!("Claim failed: {}", error_message);
-                        claim_state.set(ClaimState::Error(error_message));
+                    ClaimVersion::V2 => {
+                        match JsFuture::from(claim_v2(&address, amount)).await {
+                            Ok(result) => {
+                                match result.into_serde::<RawTransaction>() {
+                                    Ok(raw_tx) => {
+                                        if raw_tx.success {
+                                            match JsFuture::from(sendRawTransaction(&raw_tx.raw_transaction, &raw_tx.destination)).await {
+                                                Ok(_) => {
+                                                    log::info!("Transaction sent successfully");
+                                                    claim_state.set(ClaimState::Done("Transaction sent successfully".to_string()));
+                                                }
+                                                Err(e) => {
+                                                    let error_message = format!("{:?}", e);
+                                                    log::error!("Transaction failed: {}", error_message);
+                                                    claim_state.set(ClaimState::Error(error_message));
+                                                }
+                                            }
+                                        } else {
+                                            let error_message = "Failed to get raw transaction".to_string();
+                                            log::error!("{}", error_message);
+                                            claim_state.set(ClaimState::Error(error_message));
+                                        }
+                                    }
+                                    Err(e) => {
+                                        let error_message = format!("{:?}", e);
+                                        log::error!("Claim failed: {}", error_message);
+                                        claim_state.set(ClaimState::Error(error_message));
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                let error_message = format!("{:?}", e);
+                                log::error!("Claim failed: {}", error_message);
+                                claim_state.set(ClaimState::Error(error_message));
+                            }
+                        }
                     }
                 }
             });
--- src/js/claim.js
+++ src/js/claim.js
@@ -1,61 +1,119 @@
 export async function claim(address, amount) {
-  if (!Telegram.WebApp.initDataUnsafe.user) {
-    console.error("User is not available");
-    return;
-  }
-
-  const data = {
-    id: Telegram.WebApp.initDataUnsafe.user.id,
-    user: Telegram.WebApp.initDataUnsafe.user.username,
-    type: "claim",
-    address,
-    amount,
-  };
-
-  console.log("Claiming data", data);
-
-  try {
-    const response = await fetch(
-      "https://tg-api.konnektoren.help/api/v1/claim",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      },
-    );
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+    if (!Telegram.WebApp.initDataUnsafe.user) {
+        console.error("User is not available");
+        return;
     }
 
-    const result = await response.json();
+    const data = {
+        id: Telegram.WebApp.initDataUnsafe.user.id,
+        user: Telegram.WebApp.initDataUnsafe.user.username,
+        type: "claim",
+        address,
+        amount,
+    };
 
-    if (result.success) {
-      Telegram.WebApp.showPopup(
-        {
-          title: "Claimed successfully",
-          message: "Success! View transaction",
-          buttons: [
-            { id: "link", type: "default", text: "Open Explorer" },
-            { type: "cancel" },
-          ],
-        },
-        function (btn) {
-          if (btn === "link") {
-            Telegram.WebApp.openLink(result.explorer_url);
-          }
-        },
-      );
-      return Promise.resolve(result.explorer_url);
-    } else {
-      alert("Claim failed.");
-      return Promise.reject(result);
+    console.log("Claiming data", data);
+
+    try {
+        const response = await fetch(
+            "https://tg-api.konnektoren.help/api/v1/claim",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+
+        if (result.success) {
+            Telegram.WebApp.showPopup(
+                {
+                    title: "Claimed successfully",
+                    message: "Success! View transaction",
+                    buttons: [
+                        {id: "link", type: "default", text: "Open Explorer"},
+                        {type: "cancel"},
+                    ],
+                },
+                function (btn) {
+                    if (btn === "link") {
+                        Telegram.WebApp.openLink(result.explorer_url);
+                    }
+                },
+            );
+            return Promise.resolve(result.explorer_url);
+        } else {
+            alert("Claim failed.");
+            return Promise.reject(result);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Error occurred while claiming.");
+        return Promise.reject(error);
     }
-  } catch (error) {
-    console.error("Error:", error);
-    alert("Error occurred while claiming.");
-    return Promise.reject(error);
-  }
+}
+
+export async function claim_v2(address, amount) {
+    /*
+    if (!Telegram.WebApp.initDataUnsafe.user) {
+      console.error("User is not available");
+      return Promise.reject(new Error("User is not available"));
+    }
+
+    const data = {
+      id: Telegram.WebApp.initDataUnsafe.user.id,
+      user: Telegram.WebApp.initDataUnsafe.user.username,
+      type: "claim",
+      address,
+      amount,
+    };
+    */
+
+    const data = {
+        id: 1,
+        user: "user",
+        type: "claim",
+        address,
+        amount
+    };
+
+    console.log("Claiming data", data);
+
+    try {
+        const response = await fetch(
+            //"https://tg-api.konnektoren.help/api/v2/claim",
+            "http://localhost:3000/api/v2/claim",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+
+        if (result.success) {
+            return result;
+        } else {
+            alert("Claim failed.");
+            return Promise.reject(result);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Error occurred while claiming.");
+        return Promise.reject(error);
+    }
 }
