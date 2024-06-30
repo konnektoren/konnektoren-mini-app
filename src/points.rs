1e007446333e19438ba0a3e20df79ef5089d51e2
Christian M <chriamue@gmail.com>
2024-06-30 17:36:15 +0200

    :see_no_evil: add cargo lock

--- src/points.rs
+++ src/points.rs
@@ -1,7 +1,7 @@
 use crate::telegram::{get_item, set_item};
 use yew::prelude::*;
 
-pub fn add_points(points: i32) {
+pub fn add_points(points: u32) {
     log::info!("Adding points: {}", points);
     wasm_bindgen_futures::spawn_local(async move {
         match get_item("points").await {
@@ -9,7 +9,7 @@ pub fn add_points(points: i32) {
                 let points_value = result
                     .as_string()
                     .unwrap_or("0".to_string())
-                    .parse::<i32>()
+                    .parse::<u32>()
                     .unwrap_or(0);
                 let new_points = points_value + points;
                 wasm_bindgen_futures::spawn_local(async move {
